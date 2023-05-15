c12586cc9b8cafdd19a0880e5a0e410280f98c77
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-15 07:43:12 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+printf "\033c\033[44;30m\n"
+sudo apt-get install  gcc
+sudo apt-get install   make
+sudo apt-get install  bison
+sudo apt-get install  flex
+sudo apt-get install  gmp
+sudo apt-get install  mpc
+sudo apt-get install  mpfr
+sudo apt-get install  texinfo
+sudo apt-get install  isl
