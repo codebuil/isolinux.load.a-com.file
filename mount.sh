769691d17472a0b202f6e181b688fbc99167e3b2
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-19 22:31:10 +0100

    Add files via upload

---
+++ mount.sh
@@ -0,0 +1,7 @@
+dd if=/dev/zero of=out.img  bs=143k count=10
+sudo mkfs.vfat -n 'BLUE' -S 512 -f 2 -F 12 out.img
+sudo mkdir /mnt/new
+chmod 777 out.img
+sudo mount -t vfat -o loop out.img /mnt/new
+sudo cp hello  /mnt/new
+sudo umount /mnt/new
