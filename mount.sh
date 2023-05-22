e9d349825dd28c2a07669e8536d63c414db3f7a1
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-22 19:41:15 +0100

    Create a hard disk to emulates

--- mount.sh
+++ mount.sh
@@ -1,5 +1,5 @@
 printf "\ec\e[44;30m\n"
-dd if=/dev/zero of=out.img  bs=143k count=10
+dd if=/dev/zero of=out.img  bs=143k count=1000
 sudo mkfs.vfat -n '_' -S 512 -f 2 -F 12 out.img
 sudo mkdir /mnt/new
 chmod 777 out.img
