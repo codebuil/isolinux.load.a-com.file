e145ec92dd74d9e2665190971b015d8ca94fec02
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-24 23:01:21 +0100

    Delete mount.sh

--- mount.sh
+++
@@ -1,8 +0,0 @@
-printf "\ec\e[44;30m\n"
-dd if=/dev/zero of=out.img  bs=143k count=1000
-sudo mkfs.vfat -n '_' -S 512 -f 2 -F 12 out.img
-sudo mkdir /mnt/new
-chmod 777 out.img
-sudo mount -t vfat -o loop out.img /mnt/new
-sudo cp hello  /mnt/new
-sudo umount /mnt/new
