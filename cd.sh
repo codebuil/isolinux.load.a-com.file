994b2ade4045aa262d1938f312e41347da1adc2d
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-02 11:22:33 +0100

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -3,8 +3,8 @@ mkdir -p CD_root/images
 mkdir -p CD_root/kernel
 cp isolinux.bin CD_root/isolinux/isolinux.bin
 cp isolinux.cfg CD_root/isolinux/isolinux.cfg
-cp *.c32 CD_root/isolinux/
+cp *.bin CD_root/isolinux/
 cp *.com CD_root/isolinux/
-cp *.elf CD_root/isolinux/
+cp *.bss CD_root/isolinux/
 genisoimage -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
 
